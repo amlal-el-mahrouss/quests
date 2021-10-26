9d69ab48490fb8ac67794592d7e39228a3f8a318
RouteSixtySix <64403535+amllx@users.noreply.github.com>
2021-10-26 15:52:34 +0200

    Update cl_init.lua
    Add Offrande cricle

--- lua/entities/cauldron/cl_init.lua
+++ lua/entities/cauldron/cl_init.lua
@@ -4,4 +4,7 @@ include('shared.lua')
 function ENT:Draw()
 	self:DrawModel()
 
+	cam.Start3D2D(self:GetPos(), self:GetAngles(), 2)
+		surface.DrawCircle(self:GetPos().x / 2, self:GetPos().z / self:GetPos().y, 32, 0, 255, 0, 155);
+	cam.End3D2D();
 end
